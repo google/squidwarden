0b111b495707ff45b6a9e993e7b32dbd44b8043a
Thomas Habets <habets@google.com>
2016-08-27 00:23:38 +0000

    acl: Keyboard navigation

--- cmd/ui/static/acl.js
+++ cmd/ui/static/acl.js
@@ -1,14 +1,58 @@
+var selected_rule = 0;
+
 $(document).ready(function() {
     $("#acl-selection").change(function(e) {
 	window.location.href = "/acl/" + $(this).val();
     });
     $("#button-move").click(move);
+
+    $("body").keypress(keypressHandler);
+    $("table#acl-rules input.checked-rules").change(function() {checkedRulesChanged($(this))});
+    changeSelected(1);
 });
+
+function checkedRulesChanged(me) {
+    var ruleid = me.data("ruleid");
+    if (me.prop("checked")) {
+	$("#acl-rules-row-"+ruleid).addClass("selected");
+    } else {
+	$("#acl-rules-row-"+ruleid).removeClass("selected");
+    }
+}
+
+function keypressHandler(event) {
+    switch (event.which) {
+    case 106: // 'j'
+	changeSelected(1);
+	break;
+    case 107: // 'k'
+	changeSelected(-1);
+	break;
+    case 120: // 'x'
+	var o = $("#acl-rules tbody tr:nth-child("+(selected_rule+1)+") input.checked-rules");
+	o.prop("checked", !o.prop("checked"));
+	checkedRulesChanged(o);
+	break;
+    default:
+	console.log("Keypress: " + event.which);
+	break;
+    }
+}
+
+function changeSelected(delta) {
+    var o;
+    $("#acl-rules tbody tr:nth-child("+(selected_rule+1)+") td.acl-rules-row-selected").text("");
+    selected_rule += delta;
+    if (selected_rule < 0) {
+	selected_rule = 0;
+    }
+    $("#acl-rules tbody tr:nth-child("+(selected_rule+1)+") td.acl-rules-row-selected").text(">");
+}
 
 function move() {
     var rules = new Array;
     $(".checked-rules:checked").each(function(index) {
-	rules[index] = $(this).data("id");
+	rules[index] = $(this).data("ruleid");
     });
     var data = {};
     data["destination"] = $("#acl-move-selection").val();
@@ -17,7 +61,8 @@ function move() {
 	.done(function() {
 	    console.log("success");
 	    for (var i = 0; i < rules.length; i++) {
-		$("#acl-rules-row-" + rules[i]).css("display", "none");
+		$("#acl-rules-row-" + rules[i]).remove();
+		changeSelected(0);
 	    }
 	}).fail(function(o, text, error) {
 	    console.log("Failed!");
